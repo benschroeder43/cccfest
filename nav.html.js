b96bce8f74e26fe0a8d181386a474ad732c02de0
Benjamin Schroeder <bennocho@icloud.com>
2024-01-26 14:02:15 -0700

    Add files via upload

---
+++ nav.html.js
@@ -0,0 +1,50 @@
+nav = document.currentScript.parentElement
+
+names= ['Attend', 'Perform',  
+        'Contribute', 'Connect',  
+        'Create', 'Vendors',  
+        'Our Philosophy'] 
+ 
+for (name of names) 
+{ 
+nav.insertAdjacentHTML ('beforeend', `<a><span><span>${name}</span></span></a>`)
+nav.lastElementChild.children[0].addEventListener("touchstart", handle_span_touch) 
+}
+
+nav.addEventListener("touchstart", handle_nav_touch)
+
+
+
+
+function handle_span_touch (touch_ev)
+{
+touch_ev.preventDefault()
+touch_ev.stopPropagation()
+
+target = touch_ev.changedTouches[0].target.parentElement
+
+for (sib of target.parentElement.children)
+{
+if (sib == target) continue
+sib.classList.remove('expand','top')
+sib.expanded = false
+}
+
+if (target.expanded) target.classList.remove('expand')
+else target.classList.add('expand', 'top')
+target.expanded = !target.expanded
+}
+
+function handle_nav_touch (touch_ev)
+{
+
+touch_ev.preventDefault()
+target = touch_ev.changedTouches[0].target
+if (target.tagName == "NAV")
+for (sib of target.children)
+{
+sib.classList.remove('expand', 'top')
+sib.expanded = false
+}
+
+}
